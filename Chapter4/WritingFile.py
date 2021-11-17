155a239782c41900e4a380904914c8ad8c844ad3
Kang-wen7 <kangwen@graduate.utm.my>
2021-11-17 17:10:07 +0800

    Chapter3

---
+++ Chapter4/WritingFile.py
@@ -0,0 +1,8 @@
+#open file for writing
+#creates file if it does not exist
+#overwrites file if it exists
+
+temp_file = open("input.txt", "w")
+print("first line", file = temp_file)
+print("second line", file = temp_file)
+temp_file.close()
