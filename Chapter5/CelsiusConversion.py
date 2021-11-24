bd8e4b3afb52ace005c076bd0d21de67973dc963
Kang-wen7 <kangwen@graduate.utm.my>
2021-11-24 15:28:20 +0800

    Chapter5

---
+++ Chapter5/CelsiusConversion.py
@@ -0,0 +1,13 @@
+#Conversion Program
+
+def celsius_to_fahrenheit(celcius_float):
+	"""Convert Celsius to Fahrenheit"""
+	return celsius_float*1.8+3.2
+	
+#main part of the program
+print("Convert Celsius to Fahrenheit.")
+celsius_float = float(input("Enter a Celsius Temp: ")
+#call the conversion function
+celsius_to_fahrenheit(celsius_float) = fahrenheit_float
+#print the returned value
+print(celsius_float," converts to ",fahrenheit_float, " Fahrenheit ")
